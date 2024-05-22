5bac991ec90403648692e7c00c8627aa8d4c0bba
TheeradetSukprem <161497900+TheeradetSukprem@users.noreply.github.com>
2024-05-22 16:00:53 +0700

    feat: Test JavaScript

--- question-2.js
+++ question-2.js
@@ -3,4 +3,23 @@ const inventory = [
   { name: "Apple", price: 35, quantity: 100 },
   { name: "Banana", price: 10, quantity: 50 },
 ];
-// เริ่มเขียนโค้ดตรงนี้
+// เริ่มเขียนโค้ดตรงนี
+inventory[0].quantity = 200 ;
+
+inventory.push ({
+ name: "Orange", price : 20 , quantity:300 
+});
+
+let totalApple = inventory[0].price * inventory[0].quantity 
+let totalBanana = inventory[1].price * inventory[1].quantity 
+let totalOreange = inventory[2].price * inventory[2].quantity 
+
+
+let allTotalFruits = totalApple + totalBanana + totalOreange
+
+// console.log(totalApple);
+// console.log(totalBanana);
+// console.log(totalOreange);
+  console.log('มูลค่ารวมของจำนวนสินค้าทั้งหมดในสต็อก'+ ' ' + allTotalFruits + ' ' +'บาท');
+
+  // มันมีวิธีที่สั้นกว่านี้แต่ผมคิดไม่ทันเลยเอาวิธีที่ง่ายที่สุดครับ  (ใช่้ reduce)
--- question-4.js
+++ question-4.js
@@ -5,3 +5,13 @@ const inventory = [
   { name: "Orange", price: 30, quantity: 60 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+let min = inventory [0];
+for (const product of inventory) {
+  if (product.quantity < min.quantity) {
+    min = product;
+  }
+}
+
+console.log(min);
+console.log(min.quantity);
+console.log(`สินค้าที่มีจำนวนต่ำที่สุดในคลังสินค้าคือ`+ " " + min.name + " " + 'ซึ่งมี' + " " + min.quantity + " " + 'ชิ้น' );
